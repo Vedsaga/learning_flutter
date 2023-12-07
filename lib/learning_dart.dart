ba34c278509c62d4d6de435b76e9d7ddb2a925af
harshit kumar <harshit.kumar@esamudaay.com>
2023-12-07 15:18:15 +0530

    adding snippet for mixin, object, class, inherence

--- lib/learning_dart.dart
+++ lib/learning_dart.dart
@@ -23,26 +23,71 @@ var a = 1;
 /// Memory type dynamic
 dynamic b = 1;
 
-class Human {
-  /// Memory type const fixed value & datatype pre-compiled
-  /// use case of the const vars
-  /// const endpoint = 'https://api.example.com/posts';
-  static const int y = 1;
+/// Object
+late final int xxx;
+late final Object yyy;
 
-  /// Memory type final
-  final int z = 1;
+/// Object?
+late final int? aa; // ( any int type  or null)
+late final Object? bb; // ( any Object type or null)
 
-  /// Memory type var
-  var a = 1;
+// type of class & inheritance & mixin
 
-  /// Memory type dynamic
-  dynamic b = 1;
+mixin Rate {
+  void rate() {
+    print('rate');
+  }
 }
 
-/// Memory type final
-Human human = Human();
+mixin Salary {
+  void salary();
+}
 
-var updatedHuman = human.y = 2;
-var updatedHuman1 = human.z = 'hello world';
-var updatedHuman2 = human.a = 2;
-var updatedHuman4 = human.b = true;
+abstract class Human {
+  final String age;
+  const Human(this.age);
+  void name();
+  int walkSpeed() {
+    return 10;
+  }
+}
+
+/// Functions
+void work() {}
+
+abstract class Work {
+  //Methods
+  void work();
+}
+
+class Abhishek with Salary implements Human, Work {
+  @override
+  void name() {}
+
+  @override
+  int walkSpeed() {
+    return 20;
+  }
+
+  @override
+  void work() {}
+
+  @override
+  String get age => throw UnimplementedError();
+
+  @override
+  void salary() {}
+}
+
+class Harsh extends Human with Salary, Rate implements Work {
+  Harsh(super.age);
+
+  @override
+  void name() {}
+
+  @override
+  void work() {}
+
+  @override
+  void salary() {}
+}
